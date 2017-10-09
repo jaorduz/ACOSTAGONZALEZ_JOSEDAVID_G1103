9abc15d3f71a293178911e2825cb9108f4f000c8
josacosta <32560298+josacosta@users.noreply.github.com>
2017-10-09 10:59:22 -0500

    Add files via upload

---
+++ EXAMEN/examen.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main() {
+    int i,x;
+x=0;
+  while(9>=x)
+  {
+  	 
+  
+  	 
+        printf("Dame un numero y te digo si es oar o impar:\t");
+
+	scanf("%d",&i);
+
+    if (i%2==0)
+    {
+       printf("Es par.\n");
+    }
+    else
+    {
+       printf("Es impar.\n");
+    }  
+   x=x+1;
+    }
+
+return 0;
+}
+//Acosta Gonzalez Jose David
+
